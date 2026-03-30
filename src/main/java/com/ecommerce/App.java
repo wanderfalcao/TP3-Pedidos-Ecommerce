5b098d2860a9b35250e562abb07fe5ef01c28764
wanderfalcao <wander.campelo@al.infnet.edu.br>
2026-03-30 16:47:21 -0300

    refactor: extrai classe Item para substituir listas paralelas

--- src/main/java/com/ecommerce/App.java
+++ src/main/java/com/ecommerce/App.java
@@ -4,12 +4,8 @@ public class App {
     public static void main(String[] args) {
         Client client = new Client("João", "joao@email.com");
         Order order = new Order(client);
-        order.products.add("Notebook");
-        order.quantities.add(1);
-        order.prices.add(3500.0);
-        order.products.add("Mouse");
-        order.quantities.add(2);
-        order.prices.add(80.0);
+        order.addItem(new Item("Notebook", 1, 3500.0));
+        order.addItem(new Item("Mouse", 2, 80.0));
         order.printInvoice();
         order.sendEmail();
     }
--- src/main/java/com/ecommerce/Order.java
+++ src/main/java/com/ecommerce/Order.java
@@ -1,24 +1,27 @@
 package com.ecommerce;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
     private Client client;
-    public List products = new ArrayList<>();
-    public List quantities = new ArrayList<>();
-    public List prices = new ArrayList<>();
+    private List<Item> items = new ArrayList<>();
     public double discountRate = 0.1;
 
     public Order(Client client) {
         this.client = client;
     }
 
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
     public void printInvoice() {
         double total = 0;
         System.out.println("Cliente: " + client.getName());
-        for (int i = 0; i < products.size(); i++) {
-            System.out.println(quantities.get(i) + "x " + products.get(i) + " - R$" + prices.get(i));
-            total += (double) prices.get(i) * (int) quantities.get(i);
+        for (Item item : items) {
+            System.out.println(item.getQuantity() + "x " + item.getProductName() + " - R$" + item.getPrice());
+            total += item.subtotal();
         }
         System.out.println("Subtotal: R$" + total);
         System.out.println("Desconto: R$" + (total * discountRate));
